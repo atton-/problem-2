23f8c44027f12a7d632fe4460a6d3321e6e5546f
e115763 <e115763@gmail.com>
2013-02-21 10:06:26 +0900

    create AmidaLine class

--- amida.rb
+++ amida.rb
@@ -1,7 +1,45 @@
 # vim:set fileencoding=utf-8:
 
+class AmidaLine
+  @connections
+  @length
+
+  def initialize length
+    @connections = []
+    @length      = length
+  end
+
+  def connected? point
+    @connections.include? point
+  end
+
+  def connect point
+    if connected?(point)
+      raise "point #{point} is already connected"
+    end
+
+    unless (0...@length).include?(point)
+      raise "point #{point} is invalid position"
+    end
+
+    @connections << point
+  end
+end
+
+hoge = AmidaLine.new(19)
+p hoge.connected?(3)
+p hoge.connect(3)
+p hoge.connect(3)
+p hoge.connected?(3)
+p hoge.connect(18)
+p hoge.connect(39)
+
 class Amida
+  @lines
+
   def initialize num, length=10
+    @lines = []
+    num.times{@lines << AmidaLine.new(length)}
   end
 
   def display
@@ -10,5 +48,5 @@ class Amida
 end
 
 
-amida = Amida.new(3)
+amida = Amida.new(2)
 amida.display
