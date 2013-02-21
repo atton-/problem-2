3b616219cfcde8f5926cf88b63bb2f74e8e8b630
e115763 <e115763@gmail.com>
2013-02-21 10:28:01 +0900

    impl single_connect

--- amida.rb
+++ amida.rb
@@ -1,12 +1,20 @@
 # vim:set fileencoding=utf-8:
 
+require 'pp'
+
 class AmidaLine
   @connections
   @length
 
+  attr_reader :length
+
   def initialize length
     @connections = []
     @length      = length
+  end
+
+  def points
+    (0...@length).to_a
   end
 
   def connected? point
@@ -18,7 +26,7 @@ class AmidaLine
       raise "point #{point} is already connected"
     end
 
-    unless (0...@length).include?(point)
+    unless points.include?(point)
       raise "point #{point} is invalid position"
     end
 
@@ -26,27 +34,52 @@ class AmidaLine
   end
 end
 
-hoge = AmidaLine.new(19)
-p hoge.connected?(3)
-p hoge.connect(3)
-p hoge.connect(3)
-p hoge.connected?(3)
-p hoge.connect(18)
-p hoge.connect(39)
-
 class Amida
   @lines
 
-  def initialize num, length=10
+  def initialize num, line_length=10, random_lines=5
     @lines = []
-    num.times{@lines << AmidaLine.new(length)}
+    num.times{@lines << AmidaLine.new(line_length)}
+    single_connect
+    random_connect random_lines
+  end
+
+  def single_connect
+    @lines.each_cons(2) do |left, right|
+      try_connect left, right
+    end
+  end
+
+  def try_connect left_line, right_line
+    points = left_line.points
+    points.shuffle!
+
+    lines = [left_line, right_line]
+
+    points.each do |p|
+      unless lines.any?{|l|l.connected?(p)}
+        lines.each{|l|l.connect(p)}   # connect
+        return
+      end
+    end
+    raise "not found free point for single connect"
+  end
+
+  def random_connect num
   end
 
   def display
-    print "hoge"
+    pp @lines
   end
 end
 
 
-amida = Amida.new(2)
+Amida.new(9,5).display
+puts
+Amida.new(8,2).display
+puts
+Amida.new(8,1).display
+puts
+
+amida = Amida.new(10)
 amida.display
